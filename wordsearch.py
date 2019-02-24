381b61d149d0a49d022aa68bee0d5402a6488b9f
SebastianMCarreira <sebastian.m.careira@gmail.com>
2019-02-24 03:26:19 -0300

    fillRandom implemented to fill the board with random letters

--- wordsearch.py
+++ wordsearch.py
@@ -1,4 +1,10 @@
-from random import randint, sample
+from random import randint, sample, choices
+from itertools import product
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+frequencies = [0.0812, 0.0149, 0.0271, 0.0432, 0.1202, 0.023, 0.0203, 0.0592, 0.0731, 0.001, 0.0069, 0.0398, 0.0261,
+               0.0695, 0.0768, 0.0182, 0.0011, 0.0602, 0.0628, 0.091, 0.0288, 0.0111, 0.0209,0.0017, 0.0211, 0.0007]
 
 class Board():
     def __init__(self, width, height):
@@ -18,6 +24,10 @@ class Board():
         length = len(word)
         hidWord = HiddenWord(word,self.width-length,self.height-length)
 
+
+        ## TODO: Reformat this mess.s
+        ## TODO: If the board is too small, there is a chance that it will not be able to place a word and get
+        ##       stuck in a loop trying to reroll the coordinates until it can be placed.
         isInOccupiedSpaces = True
         while isInOccupiedSpaces:
             foundOneOccupied = False
@@ -28,6 +38,12 @@ class Board():
             isInOccupiedSpaces = foundOneOccupied
         for coord, letter in zip(hidWord.spaces, hidWord.word):
             self.board[coord[1]][coord[0]] = letter
+    
+    def fillRandom(self):
+        for i,j in product(range(self.height),range(self.width)):
+            if self.board[i][j] == " ":
+                self.board[i][j] = choices(population=letters, weights=frequencies)[0]
+
 
 
 
@@ -73,3 +89,6 @@ for word in words_to_search:
     print("Added: "+word)
     print(board)
     print("=======================================")
+
+board.fillRandom()
+print(board)
