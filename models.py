5610f2def1af09a8ac38ea7027cc05e77c3e3a9d
SebastianMCarreira <sebastian.m.careira@gmail.com>
2019-02-27 21:32:10 -0300

    cursor movement without full reprint and wordlist

--- models.py
+++ models.py
@@ -2,19 +2,41 @@ from random import randint, choices
 from itertools import product
 from colorama import Back, Fore
 
+import os
+
+if os.name == "nt":
+    from ctypes import windll, c_short, c_char_p, Structure
+
 LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 FREQUENCIES = [0.0812, 0.0149, 0.0271, 0.0432, 0.1202, 0.023, 0.0203, 0.0592, 0.0731, 0.001, 0.0069, 0.0398, 0.0261,
                0.0695, 0.0768, 0.0182, 0.0011, 0.0602, 0.0628, 0.091, 0.0288, 0.0111, 0.0209,0.0017, 0.0211, 0.0007]
 
+def moveConsoleCursor(x,y):
+    if os.name == "nt":
+        STD_OUTPUT_HANDLE = -11
+ 
+        class COORD(Structure):
+            pass
+        COORD._fields_ = [("X", c_short), ("Y", c_short)]
+
+        h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+        windll.kernel32.SetConsoleCursorPosition(h, COORD(x, y))
+    else:
+        print('\033[{};{}H'.format(str(x+1),str(y+1))) 
+
 class Cursor():
     def __init__(self, limit_x, limit_y):
         self.x = 0
         self.y = 0
+        self.lastX = 0
+        self.lastY = 0
         self.limit_x = limit_x
         self.limit_y = limit_y
 
     def move(self, code): #Returns True if the cursor moved
+        self.lastX = self.x
+        self.lastY = self.y
         if code == 75 and self.x != 0:
             self.x -= 1
             return True
@@ -47,6 +69,9 @@ class Board():
 
     def show(self):
         for j,i in product(range(self.height),range(self.width)):
+
+            # if i == self.cursor.lastX and j == self.cursor.lastY:
+            #     print(self.board[j][i], end=" ")
             if i == self.cursor.x and j == self.cursor.y:
                 print(Fore.RED + self.board[j][i], end=" ")
             else:
@@ -54,8 +79,13 @@ class Board():
             
             if i == self.width - 1:
                 print("")
-            
 
+    def update(self):
+        moveConsoleCursor(self.cursor.lastX*2,self.cursor.lastY)
+        print(self.board[self.cursor.lastY][self.cursor.lastX], end=" ")
+        moveConsoleCursor(self.cursor.x*2,self.cursor.y)
+        print(Fore.RED + self.board[self.cursor.y][self.cursor.x], end=" ")
+        moveConsoleCursor(self.cursor.x*2,self.cursor.y)
     
     def addWord(self, word):
         length = len(word)
@@ -75,11 +105,19 @@ class Board():
             isInOccupiedSpaces = foundOneOccupied
         for coord, letter in zip(hidWord.spaces, hidWord.word):
             self.board[coord[1]][coord[0]] = letter
+        self.hiddenWords.append(hidWord)
     
     def fillRandom(self):
         for i,j in product(range(self.height),range(self.width)):
             if self.board[i][j] == " ":
                 self.board[i][j] = choices(population=LETTERS, weights=FREQUENCIES)[0]
+
+    def showWords(self):
+        moveConsoleCursor(0,self.height)
+        print("==============================")
+        print("Words to search:")
+        for word in self.hiddenWords:
+            print("- " + word.word)
 
         
 
--- tests.py
+++ tests.py
@@ -2,12 +2,28 @@ from random import sample
 from models import HiddenWord, Board
 from colorama import init
 from time import sleep
-from os import system
 
+import os
 import keyboard
 
-init(autoreset=True)
+init(autoreset=True, convert=True, strip=True)
 
+if os.name == "nt":
+    from ctypes import windll, c_short, c_char_p, Structure
+   
+
+def moveCursor(x,y):
+    if os.name == "nt":
+        STD_OUTPUT_HANDLE = -11
+ 
+        class COORD(Structure):
+            pass
+        COORD._fields_ = [("X", c_short), ("Y", c_short)]
+
+        h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+        windll.kernel32.SetConsoleCursorPosition(h, COORD(x, y))
+    else:
+        print('\033[{};{}H'.format(str(x),str(y))) 
 
 with open("words.txt") as f:
     words = [x.strip() for x in f.readlines()]
@@ -22,13 +38,13 @@ for word in words_to_search:
     board.addWord(word)
 
 board.fillRandom()
-system('cls')
-print(board)
+os.system('cls')
+board.show()
+board.showWords()
 
 def keypress(event):
     if event.event_type == "down" and board.cursor.move(event.scan_code):
-        system('cls')
-        board.show()
+        board.update()
 
 keyboard.hook(keypress)
 keyboard.wait("esc")
