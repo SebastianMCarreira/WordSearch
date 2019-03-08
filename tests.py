528c74488f8a3834e893f0a62bfe7061ae7ac531
sebastian.carreira <sebastian.carreira@wetcom.com>
2019-03-08 14:55:37 -0300

    Deleted unused function

--- tests.py
+++ tests.py
@@ -10,20 +10,7 @@ init(autoreset=True, convert=True, strip=True)
 
 if os.name == "nt":
     from ctypes import windll, c_short, c_char_p, Structure
-   
 
-def moveCursor(x,y):
-    if os.name == "nt":
-        STD_OUTPUT_HANDLE = -11
- 
-        class COORD(Structure):
-            pass
-        COORD._fields_ = [("X", c_short), ("Y", c_short)]
-
-        h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
-        windll.kernel32.SetConsoleCursorPosition(h, COORD(x, y))
-    else:
-        print('\033[{};{}H'.format(str(x),str(y))) 
 
 with open("words.txt") as f:
     words = [x.strip() for x in f.readlines()]
