d1d02d1c05fba04f8c657926c5dc3188949d0897
SebastianMCarreira <sebastian.m.careira@gmail.com>
2019-04-10 00:12:07 -0300

    Fixed menu jump after winning game

--- tests.py
+++ tests.py
@@ -6,49 +6,28 @@ import os
 import keyboard
 
 from models import HiddenWord, Board
-from auxiliary_functions import clearConsole
+from auxiliary_functions import clearConsole, moveConsoleCursor
 
 init(autoreset=True, convert=True, strip=True)
+
+IN_MENU = True
+OPTION = 1
 
 with open("words.txt") as f:
     words = [x.strip() for x in f.readlines()]
     words_l = len(words) - 1
 
-
-def addNWords(board, n):
-    words_to_search = sample(words, n)
-    for word in words_to_search:
-        board.addWord(word)
-
-
-def promtStartMenu(board):
-    if not board:
-        board = Board(1,1)
+def promtStartMenu():
+    board = Board(1,1)
     print(board)
     clearConsole()
     print("Select difficulty:")
-    print("1) Easy (10x10, 5 words)")
-    print("2) Medium (15x15, 12 words)")
-    print("3) Hard (20x20, 25 words)")
-    print("4) Very Hard (30x30, 40 words)")
-    option = 99
-    while True:
-        
-        option = input("Your option:")
-        if option == "1":
-            board.__init__(10,10)
-            return board, 5
-        elif option == "2":
-            board.__init__(15,15)
-            return board, 12
-        elif option == "3":
-            board.__init__(20,20)
-            return board, 25
-        elif option == "4":
-            board.__init__(30,30)
-            return board, 40
-        else:
-            print("Please select a valid option")
+    print("> Easy (10x10, 5 words)")
+    print("  Medium (15x15, 12 words)")
+    print("  Hard (20x20, 25 words)")
+    print("  Very Hard (30x30, 40 words)")
+    IN_MENU = True
+    OPTION = 1
 
 # width = 10
 # height = 10
@@ -58,24 +37,64 @@ def promtStartMenu(board):
 # for word in words_to_search:
 #     board.addWord(word)
 
-board, n_words = promtStartMenu(None)
-addNWords(board, n_words)
-board.startGame()
+promtStartMenu()
+board = None
 
 def keypress(event):
+    global OPTION
+    global IN_MENU
+    global board
+
     if event.event_type == "up":
         return
 
-    if board.gameIsWon() and event.scan_code == 28:
-        clearConsole()
-        _, n_words = promtStartMenu(board)
-        addNWords(board, n_words)
-        board.startGame()
+    if IN_MENU:
+        if event.scan_code == 72 and OPTION > 1:
+            moveConsoleCursor(0,OPTION)
+            print(" ",end="")
+            OPTION -= 1
+            moveConsoleCursor(0,OPTION)
+            print(">",end="")
+        elif event.scan_code == 80 and OPTION < 4:
+            moveConsoleCursor(0,OPTION)
+            print(" ",end="")
+            OPTION += 1
+            moveConsoleCursor(0,OPTION)
+            print(">",end="")
+        elif event.scan_code == 28:
+            if OPTION == 1:
+                board = Board(10,10)
+                n_words = 5
+            elif OPTION == 2:
+                board = Board(15,15)
+                n_words = 12
+            elif OPTION == 3:
+                board = Board(20,20)
+                n_words = 25
+            elif OPTION == 4:
+                board = Board(30,30)
+                n_words = 40
+            else:
+                raise ValueError("Option variable is out of bounds: "+str(OPTION))
+            
+            words_to_search = sample(words, n_words)
+            for word in words_to_search:
+                board.addWord(word)
+            IN_MENU = False
+            board.startGame()
 
-    if board.moveCursor(event.scan_code):
-        board.update()
-    elif event.scan_code == 28:
-        board.toggleMarking()
+    else:
+        if board.gameIsWon():
+            if event.scan_code == 28:
+                IN_MENU = True
+                OPTION = 1
+                promtStartMenu()
+                return
+        else:
+            if board.moveCursor(event.scan_code):
+                board.update()
+            elif event.scan_code == 28:
+                board.toggleMarking()
 
 keyboard.hook(keypress)
 keyboard.wait("esc")
